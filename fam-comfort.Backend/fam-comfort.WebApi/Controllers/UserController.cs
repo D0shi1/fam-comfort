6dc1a0479227179b23cd84d482a124467ec14011
daniilkopot <kopot.dan@gmail.com>
2025-04-16 15:20:52 +0300

    fix(dev): add exeptions handlers

--- fam-comfort.Backend/fam-comfort.WebApi/Controllers/UserController.cs
+++ fam-comfort.Backend/fam-comfort.WebApi/Controllers/UserController.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using fam_comfort.Application.Services;
 using fam_comfort.Application.ViewModels;
 using fam_comfort.WebApi.Mapper;
@@ -12,18 +13,31 @@ public class UserController(UserService userService) : ControllerBase
     [HttpPost("register")]
     public async Task<IActionResult> Register(UserRequest request)
     {
-        var user = await userService.Register(request.Username, request.Password);
-
-        return Ok(user.ToDto());
+        try
+        {
+            var user = await userService.Register(request.Username, request.Password);
+            return Ok(user.ToDto());
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "User is already registered");
+        }
     }
     
     [HttpPost("login")]
     public async Task<IActionResult> Login(UserRequest request)
     {
-        var token = await userService.Login(request.Username, request.Password);
+        try
+        {
+            var token = await userService.Login(request.Username, request.Password);
+            
+            HttpContext.Response.Cookies.Append("token", token);
         
-        HttpContext.Response.Cookies.Append("token", token);
-        
-        return Ok();
+            return Ok();
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            return StatusCode(500, "Invalid username or password");
+        }
     }
 }
